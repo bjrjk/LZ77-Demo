db22f29eb170257e2ca5094b750a5768a5190f60
Jack Ren <bjrjk@qq.com>
2021-03-22 17:38:08 +0800

    Added Timing Code

--- LZ77-Demo/main.cpp
+++ LZ77-Demo/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <chrono>
 #include "lz77.h"
 using namespace std;
+using namespace std::chrono;
 void writeBin(string fileName, const vector<unsigned char>& data) {
 	ofstream f(fileName, ios::binary);
 	if (!f) return;
@@ -27,14 +29,24 @@ bool cmpBin(const vector<unsigned char>& data1, const vector<unsigned char>& dat
 	return true;
 }
 int main() {
+	high_resolution_clock::time_point ts, te;
+	duration<double, std::ratio<1, 1000000>> duration_ms;
 	vector<unsigned char> data;
 	vector<unsigned char> compression;
 	vector<unsigned char> origin;
 	readBin("origin.dat", data);
+	ts = high_resolution_clock::now();
 	lz77_compress(data, compression);
+	te = high_resolution_clock::now();
+	duration_ms = te - ts;
+	cout << "Compress Time: " << duration_ms.count() << "us\n";
 	writeBin("compressed.dat",compression);
+	ts = high_resolution_clock::now();
 	lz77_decompress(compression, origin);
+	te = high_resolution_clock::now();
 	writeBin("decompressed.dat", origin);
+	duration_ms = te - ts;
+	cout << "Decompress Time: " << duration_ms.count() << "us\n";
 	bool result = cmpBin(data, origin);
 	if (result)cout << "Success!";
 	else cout << "Failed!";
